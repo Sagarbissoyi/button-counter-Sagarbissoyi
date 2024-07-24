6a699faab56b96e072bff2d4b4c35fa02ebfbd08
sagarbissoyi <sagarbissoyi10@gmail.com>
2024-07-24 11:29:36 +0530

    good to go

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,19 @@
+import React, { useState } from 'react';
+import './App.css';
 
-import React from "react";
-import './../styles/App.css';
+const ClickCounter = () => {
+  const [count, setCount] = useState(0);
 
-const App = () => {
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className="container">
+      <p className="counter-text">Button clicked {count} times</p>
+      <button className="click-button" onClick={handleClick}>Click me</button>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default ClickCounter;
